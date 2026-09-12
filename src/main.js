bb7fc47c60ad83847ff78169aba136299486b9b5
Milan <132593561+MilanM-V@users.noreply.github.com>
2026-09-12 22:58:55 +0000

    feat: ajout de nouveaux éléments visuels et mise à jour des styles

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ projects.forEach((proj) => {
   card.className = "w-80 p-6 rounded-xl bg-[#161024] border border-purple-500/30 cursor-pointer transition-all duration-300 hover:border-purple-400 hover:shadow-[0_0_25px_rgba(168,85,247,0.4)] hover:-translate-y-1";
 
   card.innerHTML = `
-    <div class="w-full bg-[#362068] -mx-6 -mt-6 px-6 py-2 rounded-t-xl border-b-2 border-[#6d50af]">
+    <div class="w-100% bg-[#362068] -mx-6 -mt-6 px-6 py-2 rounded-t-xl border-b-2 border-[#6d50af]">
       <span class="text-xs font-mono text-purple-400">${proj.header}</span>
     </div>
     <h3 class="text-lg font-bold mt-2">${proj.title}</h3>
